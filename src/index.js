2f873430dadd302b54dedbb709a8f83af6bf326c
misaki0915 <cookie0915n@yahoo.co.jp>
2020-09-06 18:44:36 +0900

    Import RecoilRoot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
 import agriLine from "./img/agri_line.png";
 import Title from "./Title";
 import Game from "./Game/index";
@@ -7,13 +8,13 @@ import Search from "./Search";
 import { Image } from "./Image";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <RecoilRoot>
     <div class="game_screen">
       <Title />
       <Game />
       <Image src={agriLine} />
       <Search />
     </div>
-  </React.StrictMode>,
+  </RecoilRoot>,
   document.getElementById("root")
 );
